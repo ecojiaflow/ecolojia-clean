267ab274b2114a00a8a9b8f15159135211333c99
Salim Benamara <salim.d.benamara@gmail.com>
2025-05-30 15:52:23 +0200

    Initialize Next.js Project with Prisma

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"8iEC3yonrgMPdlPPPoIepJTx9DfKOtN5cPMXZ9r+3N0=\"\n}"
